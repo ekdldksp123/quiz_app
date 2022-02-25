1c2ab31d9f4e1ba76eb76287e891ddf9a460818f
ekdldksp123 <ekdldksp123@naver.com>
2022-02-26 02:27:44 +0900

    edit tsconfig.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://ekdldksp123.github.io/quiz_app' : '',
+  images: {
+    loader: 'akamai',
+    path: '',
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes : [ 640 , 750 , 828 , 1080 , 1200 , 1920 , 2048 , 3840 ],
+  },
 }
 
 module.exports = nextConfig
