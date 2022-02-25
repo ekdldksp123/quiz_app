c2fcb2888964f5643ceb3d00772da12692266681
ekdldksp123 <ekdldksp123@naver.com>
2022-02-26 01:00:20 +0900

    deployment automation test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <Head>
+        <title>Quiz</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+  
 }
 
 export default MyApp
