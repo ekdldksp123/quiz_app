0ce42a88b3fa20f3dbe305b6dd38dad11f388f63
ekdldksp123 <ekdldksp123@naver.com>
2022-02-26 01:19:43 +0900

    disable react in jsx scope eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,10 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Quiz!</a>
         </h1>
-
        </main>
     </div>
   )
