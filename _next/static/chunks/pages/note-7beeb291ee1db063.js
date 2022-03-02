1608ff83849580c5b4dd300f1976184e71c5fff8
ekdldksp123 <64599394+ekdldksp123@users.noreply.github.com>
2022-03-02 19:23:54 +0000

    Deploying to gh-pages from @ ekdldksp123/quiz_app@f977bf1309998f9550904e247272e151ff178be8 🚀

---
+++ _next/static/chunks/pages/note-7beeb291ee1db063.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[614],{89:function(n,u,t){"use strict";t.r(u);var r=t(5893);u.default=function(){return(0,r.jsx)(r.Fragment,{})}},3951:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/note",function(){return t(89)}])}},function(n){n.O(0,[774,888,179],(function(){return u=3951,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
